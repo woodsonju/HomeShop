371449185b213842b6a04f66626db28d6858461e
WoodsonJ <j.woodson@hotmail.fr>
2019-07-20 13:11:32 +0200

    Add Interface Writer Completed Class Bill with methods getTotal() and generate(...) Completed Class Test for methods getTotal() and generate(...)

--- src/main/java/homeshop/Bill.java
+++ src/main/java/homeshop/Bill.java
@@ -1,14 +1,17 @@
 package homeshop;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Bill {
     
     private Customer customer;
-    private Map<Product, Integer> products;
+    private Map<Product, Integer> products = new HashMap<Product, Integer>();
+    private Delivery delivery;
 
-    public Bill(Customer customer) {
+    public Bill(Customer customer, Delivery delivery) {
         this.customer = customer;
+        this.delivery = delivery;
     }
 
     public Customer getCustomer() {
@@ -25,7 +28,53 @@ public class Bill {
      * @param product product to add
      * @param quantity quantity of the product
      */
-    public void addProduct(Product product, Integer quantity) {
+    public void addProduct(Product product, Integer quantity)
+    {
         this.products.put(product, quantity);
+    }
+
+    /**
+     * Generate an output fot the current Bill
+     * @param writer object in charge of writin
+     */
+    public void generate(Writer writer) {
+        writer.start();
+        writer.writeLine("HomeShop compagnie");
+        writer.writeLine("1 Place Charles de Gaule, 75008 Paris");
+        writer.writeLine("");
+        writer.writeLine("Facture à l'attention de : ");
+        writer.writeLine(customer.getFullname());
+        writer.writeLine(customer.getAddress());
+        writer.writeLine("");
+        writer.writeLine("Mode de livraison : " + delivery.getInfo());
+        writer.writeLine("");
+        writer.writeLine("Produit");
+        writer.writeLine("--------------------------------------------------------------");
+        for(Map.Entry<Product, Integer> entry: products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+            writer.writeLine(product.getName() + " - " + product.getPrice() + " - " + quantity + "unité(s)");
+            writer.writeLine(product.getDescription());
+            writer.writeLine("");
+        }
+        writer.writeLine("Livraison : " + delivery.getPrice());
+        writer.writeLine("--------------------------------------------------------------");
+        writer.writeLine("Total : " + this.getTotal());
+        writer.stop();
+    }
+
+
+    /**
+     * get total price for the current bill, including products and delivery cost
+     * @return total price
+     */
+    public double getTotal() {
+        double total = 0;
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            Product product = entry.getKey();
+            Integer quantity = entry.getValue();
+            total += product.getPrice()*quantity;
+        }
+        return total;
     }
 }
--- src/test/java/homeshop/BillTest.java
+++ src/test/java/homeshop/BillTest.java
@@ -1,7 +1,47 @@
 package homeshop;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BillTest {
+    
+    private String output = "";
+    
+    private Writer writerMock = new Writer() {
+        public void start() {
+            output="";
+        }
+        public void writeLine(String line) {
+            output += line + "%n";
+        }
+        public void stop() {
 
+        }
+    };
+
+    private Product cafe = new Product("Philips HD7866/61", "Philips SENSEO Quadrante, Noir - 1 ou 2 tasses", 79.99);
+    private Product tv = new Television("TV Samsung UE49MU6292", "Smart TV LED incurvée 49\"", 599, 49, "LED");
+    private Fridge fridge = new Fridge("BEKO TSE 1042 F", "Réfrigérateur BEKO 130L - Classe A+ - blanc", 189, 130, false);
+    private Customer customer = new Customer("Juste Leblanc", "19 rue Germain Pilon, Paris");
+    private Delivery lowCostRelayDelivery = new RelayDelivery(27);
+   
+    @Test
+    public void When_2productsAndDelivery_When_generatinBill_Then_getGoodLineNumber() {
+        Bill bill = new Bill(customer, lowCostRelayDelivery);
+        bill.addProduct(cafe, 1);
+        bill.addProduct(tv, 1);
+        bill.generate(writerMock);
+        int linerNumber = output.split("%n").length;
+        assertEquals(20, linerNumber);
+    }
+
+    @Test
+    public void When_3productsAndDelivery_When_generatinBill_Then_getGoodTotal() {
+        Bill bill = new Bill(customer, lowCostRelayDelivery);
+        bill.addProduct(cafe, 1);
+        bill.addProduct(tv, 1);
+        bill.addProduct(fridge, 1);
+        assertEquals(867.99, bill.getTotal(), 0.01);
+    }
 }
